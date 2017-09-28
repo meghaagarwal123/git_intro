0199dceb3ea80d98f61b5f59397987f011f158a3
deeksha004 <deekshaaggarwal@gmail.com>
2017-09-29 05:00:46 +0530

    more git info

--- myfirst.js
+++ myfirst.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello!');
+    res.end('Hello u hi!');
 }).listen(8080);
